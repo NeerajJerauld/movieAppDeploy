7805b2d818bc0ad709de2009ed15f671dba92192
Neeraj Jerauld <neeraj.jerauld@gmail.com>
2023-06-28 07:43:13 -0500

    6th commit

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 // importing
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 // const { create, readAll, remove, update,simpleFunction } = require('./crudoperator')
 
 //initialization
 const app = express();
-const movieRoute = require('./src/Routes/movieRoute')
-const path = require('path');
-
+const movieRoute = require("./src/Routes/movieRoute");
+const path = require("path");
 
 //middleware
 app.use(cors());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'/build')));
-
+console.log(__dirname)
+app.use(express.static(path.join(__dirname, "/build")));
 
 // Direct the request to movieRoute.js
-app.use('/api/movie',movieRoute)
-
+app.use("/api/movie", movieRoute);
 
 //port
-app.get('/*', function(req, res) { 
-    res.sendFile(path.join(__dirname 
-    ,'/build/index.html')); });
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/build/index.html"));
+});
 
-
-app.listen(3000,()=>{
-    console.log("app is working on port 3000");
-
-})
+app.listen(3000, () => {
+  console.log("app is working on port 3000");
+});
